dd0db7fc2503265050a16d73c60745ef6ac257c4
Seyma Kaya <seyma.kaya2@hva.nl>
2022-11-29 09:11:58 +0100

    html/css

--- assets/jsSeyma/match.js
+++ assets/jsSeyma/match.js
@@ -1,4 +1,20 @@
+FYSCloud.API.queryDatabase(
+    "SELECT * FROM message"
+).then(function(data) {
+    console.log(data);
+}).catch(function(reason) {
+    console.log(reason);
+});
+
 window.onload = test;
 function test(){
+
+}
+
+$connection = FYSCloud.API.queryDatabase("#","#")
+$db = FYSCloud($connection, "#")
+
+if(isset($_post['insert'])){
+    $radio = $_post ['radio']
 
 }
