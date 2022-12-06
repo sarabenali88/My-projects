9efd3ea415e3c967dcae53cc6f3819c3d1ba90cd
benalis <sara.benali@hva.nl>
2022-12-06 08:37:49 +0100

    html/css

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
+
+FYSCloud.API.configure({
+    url: "https://api.fys.cloud",
+    apiKey: "fys_is101_1.JGn3c54bBnlw5GsL",
+    database: "fys_is101_1_sara",
+    environment: "mockup"
+});
