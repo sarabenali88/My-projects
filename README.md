cca263071b6bb697d09e859ad4a136a0de1298f0
Seyma Kaya <seyma.kaya2@hva.nl>
2022-12-20 09:06:10 +0100

    html/css

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Onze webpagina mist de volgende onderdelen:
 
-* Javascript, er is een begin gemaakt aan javascript, maar niks ervan werkt 
-* Connectie aan de database, er is een database ontwikkeld, maar hier is geen connectie mee gemaakt
+* Javascript, er is een begin gemaakt aan javascript, maar niks ervan werkt.
+* Connectie aan de database, er is een database ontwikkeld, maar hier is nog geen gebruik van gemaakt.
 * Alle pagina's koppelen met elkaar, de pagina's die gekoppeld moeten worden met elkaar door middel
 van de nav bar bijvoorbeeld zijn gekoppeld. Sommige pagina's worden een pop-up pagina of een pagina 
 waar je alleen toegang voor hebt als je bepaalde informatie hebt verstuurd naar de console. Deze kunt u 
--- assets/jsSeyma/match.js
+++ assets/jsSeyma/match.js
@@ -1,20 +1,34 @@
-FYSCloud.API.queryDatabase(
-    "SELECT * FROM message"
-).then(function(data) {
-    console.log(data);
-}).catch(function(reason) {
-    console.log(reason);
-});
+import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
 
-window.onload = test;
-function test(){
 
-}
+    let button = document.getElementById("buttonForm")
+    button.onclick = function (event) {
+        event.preventDefault();
 
-$connection = FYSCloud.API.queryDatabase("#","#")
-$db = FYSCloud($connection, "#")
+        // function submit() {
+        const land = document.getElementById("country").value
+        const month = document.getElementById("months").value
+        const hobby = document.getElementById("hobby").value
 
-if(isset($_post['insert'])){
-    $radio = $_post ['radio']
+        console.log(land);
+        console.log(month);
+        console.log(hobby);
 
-}
+        FYSCloud.API.queryDatabase(
+            "UPDATE antwoord SET antwoord1 = ?, antwoord2 = ?, antwoord3 = ? WHERE (`idAntwoord` = '3') AND (`idGebruiker` = '1')",
+            [land, month, hobby]
+        ).then(function (data) {
+            console.log(data);
+            FYSCloud.URL.redirect("friendrequest.html");
+        }).catch(function (reason) {
+            console.log(reason);
+        });
+
+    }
+
+
+
+
+
+
+
